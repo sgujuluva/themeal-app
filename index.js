e387da62d0aec38d3edf8db94d65596611a6ecf1
sgujuluva <sanghee04@gmail.com>
2022-06-27 09:04:12 +0200

    fetching the mealdb API

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ let wrapper = document.querySelector("#wrapper");
 let image = document.querySelector("#image");
 let ingredients = document.querySelector("#ingredients");
 let instructions = document.querySelector("#instructions");
+
+// fetching the meal api
+async function getAllPosts(){
+    const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+    const items = await response.json();
+    console.log(items)
+    items.forEach(element => {
+        
+        
+    });
+
+}
