39d0dbe1f7201b5fbb13163b0a61ee75a7a9cd0a
sgujuluva <sanghee04@gmail.com>
2022-06-27 08:52:39 +0200

    selected the elements in js

---
+++ index.js
@@ -0,0 +1,6 @@
+//selecting all the elements
+let imageWrapper = document.querySelector("#imagewrapper");
+let wrapper = document.querySelector("#wrapper");
+let image = document.querySelector("#image");
+let ingredients = document.querySelector("#ingredients");
+let instructions = document.querySelector("#instructions");
