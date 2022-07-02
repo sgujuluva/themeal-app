bb81562b55ea4ef3966e5459d8be02068fe55819
sgujuluva <sanghee04@gmail.com>
2022-07-02 14:57:25 +0200

    youtube added

--- index.js
+++ index.js
@@ -1,19 +1,62 @@
-//selecting all the elements
-let imageWrapper = document.querySelector("#imagewrapper");
-let wrapper = document.querySelector("#wrapper");
-let image = document.querySelector("#image");
-let ingredients = document.querySelector("#ingredients");
-let instructions = document.querySelector("#instructions");
-
+ //selecting all the elements
+let container = document.querySelector("#container")
+let title = document.querySelector("#title");
 // fetching the meal api
 async function getAllPosts() {
-  const response = await fetch(
+  let response = await fetch(
     "https://www.themealdb.com/api/json/v1/1/random.php"
   );
-  const items = await response.json();
-  console.log(items);
- addTitle(items.data.meals[0].strMeal,
-    items.data.meals[0].strArea,items.data.meals[0].strCategory,
-    items.data.meals[0].strInstructions,
-    items.data.meals[0].strCategory)
+  const json = await response.json();
+   console.log(json); 
+
+title.innerText = json.meals.map((p) => p.strMeal)
+
+//jsx
+let div = `<div>
+
+<h2><span>Meal Category:</span>${json.meals.map((p) => p.strCategory)}</h2>
+<h3><span>Country:</span>${json.meals.map((p) => p.strArea)}</h3>
+<img src = ${json.meals.map((p) => p.strMealThumb)}></img>
+
+<p><span>Instructions:</span>${json.meals.map((p) => p.strInstructions)}</p>
+<iframe  src = ${`https://www.youtube.com/embed/${(json.meals.map(p => p.strYoutube))[0].slice(-11)}`}
+
+>   </iframe>
+</div>`
+container.innerHTML += div;
+}
+getAllPosts(); 
+
+/* let container = document.querySelector("#container");
+async function randomMeal() {
+  let fetching = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/random.php"
+  );
+  let json = await fetching.json();
+  let arr = [];
+  for (let i = 1; i <= 20; i++) {
+    if (json.meals[0][`strIngredient${i}`]) {
+      let element = `<li>${json.meals[0][`strIngredient${i}`]}</li>`;
+      arr.push(element);
+    } 
   }
+
+  // JSX
+  let div = `<div>
+    <h1><span>Meal: </span> ${json.meals.map((p) => p.strMeal)}</h1>
+    <h2><span>Category: </span>${json.meals.map((p) => p.strCategory)}</h2>
+    <h3><span>Country: </span>${json.meals.map((p) => p.strArea)}</h3>
+    <img src=${json.meals.map((p) => p.strMealThumb)}>
+    <ul><span>Ingredient: </span>${arr.join(" ")}</ul>
+    <p><span>Instructions: </span>${json.meals.map(
+      (p) => p.strInstructions
+    )} </p>
+    </div>`;
+
+  container.innerHTML += div;
+    }
+randomMeal();
+
+ <h1><span>Meal :</span> ${json.meals.map((p) => p.strMeal)}</h1> 
+ 
+ <ul><span></span>${(json.meals.map((p) => handleIngredient(p)))}</ul>*/
