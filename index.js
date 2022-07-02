90ba3cd611d38571adcc57ab8fccecb4450eb24f
sgujuluva <sanghee04@gmail.com>
2022-07-02 13:51:16 +0200

    adding titles

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ let ingredients = document.querySelector("#ingredients");
 let instructions = document.querySelector("#instructions");
 
 // fetching the meal api
-async function getAllPosts(){
-    const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
-    const items = await response.json();
-    console.log(items)
-    items.forEach(element => {
-        
-        
-    });
-
-}
+async function getAllPosts() {
+  const response = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/random.php"
+  );
+  const items = await response.json();
+  console.log(items);
+ addTitle(items.data.meals[0].strMeal,
+    items.data.meals[0].strArea,items.data.meals[0].strCategory,
+    items.data.meals[0].strInstructions,
+    items.data.meals[0].strCategory)
+  }
